bae8f2e5ab6720eefbda683c7d3a8f438621af25
bitopan987 <bitopanxon1@gmail.com>
2022-04-13 03:53:23 +0530

    Update the UI of Articles component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,21 +4,25 @@ import Loader from './Loader';
 import UserContext from '../context/UserContext';
 
 function Articles(props) {
-  console.log(props, 'vbhghgf');
-  function getDate(date) {
+  const getDate = (date) => {
     let newDate = new Date(date).toISOString().split('T')[0];
     return newDate;
-  }
+  };
+
   const userData = useContext(UserContext);
   const { isLoggedIn } = userData;
 
-  let { articles, error } = props;
+  const { articles, error } = props;
   if (error) {
     return <h2 className="text-red-500 text-center text-xl mt-8">{error}</h2>;
   }
 
   if (!articles) {
-    return <Loader />;
+    return (
+      <div className="pt-16">
+        <Loader />
+      </div>
+    );
   }
   if (!articles.length) {
     return (
@@ -33,17 +37,19 @@ function Articles(props) {
         return (
           <div
             key={article.slug}
-            className="bg-gray-200 flex justify-between flex-col mb-10 w-full p-4 rounded-md shadow-md"
+            className="bg-gray-100 flex justify-between flex-col mb-10 w-full p-4 rounded-md shadow-md"
           >
             <div className="flex justify-between w-full">
-              <div className="flex items-center my-2">
-                <Link to={`/profiles/${article.author.username}`}>
-                  <img
-                    src={article.author.image || 'smiley.png'}
-                    alt={article.author.username}
-                    className="w-14 h-14 rounded-full object-cover"
-                  />
-                </Link>
+              <Link
+                to={`/profiles/${article.author.username}`}
+                className="flex items-center my-2"
+              >
+                <img
+                  src={article.author.image || '/smiley.jpg'}
+                  alt={article.author.username}
+                  className="w-14 h-14 rounded-full object-cover"
+                />
+
                 <div className="ml-4">
                   <h5 className="text-gray-500 font-bold text-xl mr-5">
                     {article.author.username}
@@ -52,14 +58,15 @@ function Articles(props) {
                     {getDate(article.createdAt)}
                   </h6>
                 </div>
-              </div>
+              </Link>
+
               {isLoggedIn && (
-                <div className="flex items-center text-xl border-gray-500 px-3 py-1 rounded-full shadow-lg bg-gray-400">
+                <div className="flex items-center text-xl border-gray-500 px-4 py-1 rounded-full shadow-lg ">
                   <i
                     className={
                       article.favorited
-                        ? 'fas fa-heart text-red-700'
-                        : 'far fa-heart cursor-pointer text-gray-700'
+                        ? 'ri-heart-fill text-red-700'
+                        : 'ri-heart-fill cursor-pointer text-gray-700'
                     }
                     onClick={(e) => props.handleFavorite(e)}
                     data-id={article.favorited}
@@ -77,7 +84,7 @@ function Articles(props) {
               {article.description}
             </p>
             <Link to={`/articles/${article.slug}`}>
-              <h4 className="btn btn-primary inline-block">Read More</h4>
+              <h4 className=" btn-primary inline-block">Read More</h4>
             </Link>
           </div>
         );
